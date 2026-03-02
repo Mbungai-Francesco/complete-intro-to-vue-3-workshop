47037a10c0ded444defb5d9feb842208d7ad1ab6
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2026-03-02 23:27:56 +0100

    moved code to strachPad

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,72 +1,7 @@
-<script lang="ts">
-import VueCounter from './components/VueCounter.vue'
-import UserCard from '../../sandbox/src/components/UserCard.vue'
+<script setup lang="ts">
 
-export default {
-  components: {
-    VueCounter,
-    UserCard,
-  },
-  data() {
-    return {
-      message: 'Hello Vue!',
-      listOfNumbs: [
-        {
-          name: 0,
-          id: 'b674c468-5b91-49cb-b5a3-5981837ed977',
-          list: [1, 2, 3],
-        },
-        {
-          name: 1,
-          id: 'd85a011b-f95b-47bf-b7a8-504b46e6cc96',
-          list: [1, 2, 3],
-        },
-        {
-          name: 2,
-          id: 'e697c72a-6c8c-495a-b76a-64a7238b4d21',
-          list: [1, 2, 3],
-        },
-        {
-          name: 3,
-          id: 'bcd679d9-3898-468d-a2b6-7adc95130802',
-          list: [1, 2, 3],
-        },
-        {
-          name: 4,
-          id: 'a0b25edd-ed61-4e76-8fac-0bc7c07bb053',
-          list: [1, 2, 3],
-        },
-      ],
-      userData: {
-        name: 'Ben',
-        preferredFramework: 'Vue',
-        favoriteFood: 'sushi',
-        favoriteNumbers: [8, 10, 12],
-      },
-    }
-  },
-  methods:{
-    changeName() {
-      this.userData.name = "Charlie"
-    }
-  }
-}
 </script>
 
 <template>
-  <UserCard :user="userData" @change-name="changeName"/>
-  <VueCounter />
-  <!-- ! exercise 1 -->
-  <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
-  <p v-else>Odd : {{ message }}</p>
-  <ul>
-    <li v-for="val in listOfNumbs" :key="val.id">
-      {{ val.name }}
-      <ul>
-        <li v-for="(num, index) in val.list" :key="index">{{ num }}</li>
-      </ul>
-    </li>
-  </ul>
+  <p>New App</p>
 </template>
-
-<style scoped></style>
---
+++ sandbox/src/components/BaseLayout.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="wrapper">
+      <aside class="sidebar">
+        <slot name="sidebar"></slot>
+      </aside>
+      <main>
+        <slot name="main"></slot>
+      </main>
+    </div>
+    <footer>
+      <slot name="foot"></slot>
+    </footer>
+  </div>
+</template>
+
+<style>
+.sidebar {
+  border: 2px solid red;
+}
+main {
+  border: 2px solid green;
+}
+footer {
+  border: 2px solid blue;
+}
+</style>
